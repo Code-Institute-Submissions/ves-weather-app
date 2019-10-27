0604afe2f28abe0c939c0d424aba5760a8aa766e
Ubuntu <ubuntu@ip-172-31-83-24.ec2.internal>
2019-10-27 18:15:14 +0000

    Prepping the settings and requirements files for deployment

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -73,13 +74,16 @@ WSGI_APPLICATION = 'weatherapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse("postgres://pyzutbuoorfbhq:bbd04cbd04541cccd9ba2fe84ab9a7055800ddac1c058e9100c7e28979789411@ec2-54-247-72-30.eu-west-1.compute.amazonaws.com:5432/d5vudm7ors7e2k")
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
