e1d995b68daa3f7efbffd5519054d5981e6a7512
Ubuntu <ubuntu@ip-172-31-83-24.ec2.internal>
2019-11-03 22:43:50 +0000

    Add weather view.

--- user_homepage/views.py
+++ user_homepage/views.py
@@ -1,24 +1,29 @@
 import requests
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
-from user_homepage.forms import PostCity
+from .models import City
+from .forms import Form
 
 # Create your views here.
-def user_homepage(request):
+def user_homepage(request, name="userhomepage"):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=38e4fec38e509c018629074ac1754906'
-    city = 'London'
-    
+    if request.method=="GET":
+        Form = Form
+        city = request.get["city_name"]
+    else:
+        Form = Form
+        
     r = requests.get(url.format(city)).json()
-    
+            
     city_weather = {
         'city': city,
         'temperature' : r['main']['temp'],
         'description' : r['weather'][0]['description'],
         'icon' :  r['weather'][0]['icon'],
     }
-    
+            
     result = {'city_weather' : city_weather}
-    return render(request, 'userhomepage.html', result)
+    return render(request, 'userhomepage.html', result, {'Form': Form})
     
 # def post(self, request):
 
