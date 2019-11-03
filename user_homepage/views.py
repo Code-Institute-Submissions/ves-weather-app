5e26cf3edf825dadce12c52e02d76ea5a664e97b
Ubuntu <ubuntu@ip-172-31-83-24.ec2.internal>
2019-11-03 18:19:41 +0000

    Set up API functionality. Displays given city weather.

--- user_homepage/views.py
+++ user_homepage/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+import requests
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from user_homepage.forms import PostCity
 
 # Create your views here.
 def user_homepage(request):
-    return render(request, 'userhomepage.html')
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=38e4fec38e509c018629074ac1754906'
+    city = 'London'
     
-def index(request):
-    return render(request, 'base.html')
+    r = requests.get(url.format(city)).json()
+    
+    city_weather = {
+        'city': city,
+        'temperature' : r['main']['temp'],
+        'description' : r['weather'][0]['description'],
+        'icon' :  r['weather'][0]['icon'],
+    }
+    
+    result = {'city_weather' : city_weather}
+    return render(request, 'userhomepage.html', result)
+    
+# def post(self, request):
+
+    
