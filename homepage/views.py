23ad7f6af863dad03c1983cb1a5dc187bdb9fb3d
Ubuntu <ubuntu@ip-172-31-83-24.ec2.internal>
2019-11-10 16:24:24 +0000

    Add cache and metadata.

---
+++ homepage/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from homepage.forms import UserLoginForm
+
+# Create your views here.
+def index(request):
+    """Returns the index.html file"""
+    return render(request, 'index.html')
+
+def login(request):
+    """Return a login page"""
+    if request.method=="POST":
+        login_form = UserLoginForm(request.POST)
+        
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+                                    password=request.POST['password'])
+                                    
+            
+            if user:
+                auth.login(user=user, request=request)
+                return redirect("user_homepage")
+            
+            else:
+                login_form.add_error(None, "Your username or password is incorrect")
+    else:
+        login_form = UserLoginForm()
+    return render(request, "login.html", {"login_form": login_form})
+    
